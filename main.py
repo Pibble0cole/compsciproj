3328a5339cf77768d4d08c9649b718f4765a2869
Pibble0cole <8846256@philasd.org>
2026-02-25 13:01:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,29 +2,29 @@
 import tkinter as tk
 from service_opp import Service_opp
 
+#this is the list of interests
 intrests= ["animals", "cleaning", "food", "helping"]
 
 service_opp=["spca","park","soup kichen", "nursing home"]
 
 root= tk.Tk()
+root.title("Community Service Suggestion Program")
 root.geometry("1500x800")
 root.configure(bg="lightblue")
-title = tk.Label(root, text="Community Service Tracker", font=("Helvetica", 30))
+title = tk.Label(root, text="★ Community Service Tracker ★", fg="yellow", bg="blue", font=("Comic Sans MS", 30))
 title.grid(row=0, column=0, padx=10, pady=10)
 
-description = tk.Label(root, text="This is an app that helps you keep track of your community service hours \nand suggests places to start your community service work depending on your interests.", font=("Helvetica", 12))
+description = tk.Label(root, text="This is an app that helps you keep track of your community service hours \nand suggests places to start your community service work depending on your interests.", fg="white", bg="blue", font=("Helvetica", 12))
 description.grid(row=1, column=0, padx=10, pady=10,sticky="w")
 """
 frame = tk.Frame(root, width=150, height=10)
 frame.pack(side="left", anchor="nw", pady=10)
 """
 
-#Label for the name input
-name_label = tk.Label(root, text="Name:")
+#widgets for the name input
+name_label = tk.Label(root, fg="yellow", bg="blue", text="• Name:")
 name_label.grid(row=3, column=0, padx=10, pady=10,sticky="w")
 
-
-#entry
 name_entry = tk.Entry(root)
 name_entry.grid(row=4, column=0, padx=10, pady=10,sticky="w")
 
@@ -34,18 +34,19 @@ if usrname == "":
 else:
     print("proceed", usrname)
 
-#intrest selection label
-intrests_des= tk.Label(root, text="select your intrests:")
+#widgets for the selection
+intrests_des= tk.Label(root, text="• Select your intrests:", fg="yellow", bg="blue",)
 intrests_des.grid(row=5, column=0, padx=10, pady=10,sticky="w")
 
 row = 6
 tempjobs = []
 serviceobj = []
 
+#this gets the info from the text document in the folder
 with open("service.txt", "r") as file:
     tempjobs = file.readlines()
 
-
+#this splits the data and makes  it more readable
 for i in range(1,len(tempjobs)):
     line = tempjobs[i].split(";")
     name = line[0]
@@ -63,11 +64,11 @@ for opp in serviceobj:
 selected_intrests = {}
 
 
-#this loops through the list to create widgets
+#this loops through the list to create checkbox widgets
 for item in intrests:
     var = tk.IntVar()
     
-    cb = tk.Checkbutton(root, text=item, variable=var)
+    cb = tk.Checkbutton(root, text=item, fg="black", bg="blue", variable=var)
     cb.grid(row=row, column=0, padx=10, pady=10,sticky="w")
     
     
@@ -79,14 +80,14 @@ cbval = cb.cget("variable")
 if cbval == 0:
         print("please select one of the options")
 
-#Function to see suggestions
+#this is where it collects all of the suggestions to print it into a listbox widget
 suggestion_dic={}
 def get_selected(row):
     name = name_entry.get()
     print(name)
     selected = [name for name, var in selected_intrests.items() if var.get() == 1]
     row+=1
-    sug_title = tk.Label(root, text="Suggestions:")
+    sug_title = tk.Label(root, fg="yellow", bg="blue", text="Suggestions:")
     sug_title.grid(row=row, column=0, padx=10, pady=10,sticky="w")
     suggestedservice = []
     for item in selected:
@@ -97,47 +98,15 @@ def get_selected(row):
                 
     row+=1
                 
-
+#this is the lisbox widget
     listbox = tk.Listbox(root, height=12, width=150, selectmode=tk.SINGLE) 
     listbox.grid(row=row, column=0, padx=10, pady=10,sticky="w")
     for item in suggestedservice:
         listbox.insert(tk.END, item)
 
 
-
-
-
-
-#work in progress code
-    
-    """for intrest in selected:
-        row+=1
-        if intrest == "animals":
-            var = tk.IntVar()
-        
-            cb = tk.Checkbutton(root, text=service_opp[0], variable=var)
-            cb.grid(row=row, column=0, padx=10, pady=10,sticky="w")
-    
-        # Store the variable in our dictionary using the topping name as the key
-            suggestion_dic[service_opp[0]] = var
-        if intrest == "cleaning":
-            var = tk.IntVar()
-        
-            cb = tk.Checkbutton(root, text=service_opp[1], variable=var)
-            cb.grid(row=row, column=0, padx=10, pady=10,sticky="w")
-    
-        # Store the variable in our dictionary using the topping name as the key
-            suggestion_dic[service_opp[1]] = var
-        
-"""
-
-
-
-
-
-
 #this adds the submit button function
-tk.Button(root, text="Submit", command=lambda: get_selected(row)).grid(row=row, column=0, padx=10, pady=10,sticky="w")
+tk.Button(root, text="Submit", fg="yellow", bg="blue", command=lambda: get_selected(row)).grid(row=row, column=0, padx=10, pady=10,sticky="w")
 
 
 root.mainloop()
